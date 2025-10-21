3aceedf196c1b028a5c727037457c6e8e43c3536
asmaa0412 <20242043@stud.fci-cu.edu.eg>
2025-10-22 00:34:24 +0300

    Implement looping feature in PlayerAudio
    Added looping functionality to PlayerAudio class.

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -1,43 +1,51 @@
 #include "PlayerAudio.h"
 
-
 PlayerAudio::PlayerAudio()
-  {
+{
   formatManager.registerBasicFormats();
-  }
+}
 
 PlayerAudio::~PlayerAudio()
-  {
-      transportSource.setSource(nullptr);
-  }
+{
+  transportSource.setSource(nullptr);
+}
 
 void PlayerAudio::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
-  {
-    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
-  }
+{
+  transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
 
 void PlayerAudio::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+  transportSource.getNextAudioBlock(bufferToFill);
+
+  if (!looping && !transportSource.isPlaying()  //task 4
+      && getPosition() >= getLength() - 0.05)
   {
-    transportSource.getNextAudioBlock(bufferToFill);
+    stop();
   }
+}
 
 void PlayerAudio::releaseResources()
-  {
-    transportSource.releaseResources();
-  }
+{
+  transportSource.releaseResources();
+}
+
 bool PlayerAudio::LoadFile(const juce::File& file)
 {
-      if (auto* reader = formatManager.createReaderFor(file))
-      {
-        transportSource.stop();
-        transportSource.setSource(nullptr);
-        readerSource.reset();
+  if (auto* reader = formatManager.createReaderFor(file))
+  {
+    transportSource.stop();
+    transportSource.setSource(nullptr);
+    readerSource.reset();
 
-        readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
-        transportSource.setSource(readerSource.get(),0,nullptr,reader->sampleRate);
-        return true;
-      }
-      return false;
+    readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+    readerSource->setLooping(false);
+    transportSource.setSource(readerSource.get(), 0, nullptr, reader->sampleRate);
+    return true;
+  }
+
+  return false;
 }
 
 void PlayerAudio::play()
@@ -63,10 +71,22 @@ void PlayerAudio::setPosition(double pos)
 
 double PlayerAudio::getPosition() const
 {
-    return transportSource.getCurrentPosition();
+  return transportSource.getCurrentPosition();
 }
 
 double PlayerAudio::getLength() const
 {
-    return transportSource.getLengthInSeconds();
+  return transportSource.getLengthInSeconds();
+}
+
+void PlayerAudio::setLooping(bool shouldLoop)
+{
+  looping = shouldLoop;
+  if (readerSource)
+    readerSource->setLooping(looping);
+}
+
+bool PlayerAudio::getIsLooping() const
+{
+  return looping;
 }
