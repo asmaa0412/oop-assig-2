da57d912105cdbacc0c6f3d4be29188a3428cb13
asmaa0412 <20242043@stud.fci-cu.edu.eg>
2025-10-22 00:35:21 +0300

    Add loop button to PlayerGui
    Added a loop button to the PlayerGui class.

--- PlayerGui.h
+++ PlayerGui.h
@@ -18,17 +18,21 @@ class PlayerGui : public juce::Component,
 
   private:
     PlayerAudio playerAudio;
+    //Gui elements
     juce::TextButton loadButton { "Load" };
     juce::TextButton playButton { "play" };
     juce::TextButton stopButton { "stop" };
+    juce::TextButton loopButton { "Loop" };  //task 4
     juce::Slider volumeSlider;
+
+
 
     std::unique_ptr<juce::FileChooser>filechooser;
 
+    //events handlers
 
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGui)
-
     };
