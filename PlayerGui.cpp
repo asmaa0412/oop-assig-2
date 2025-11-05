1e9f23a2faa47289fe5acd4af28f3037ac535172
asmaa0412 <20242043@stud.fci-cu.edu.eg>
2025-11-05 20:31:34 +0200

    Update PlayerGui.cpp

--- PlayerGui.cpp
+++ PlayerGui.cpp
@@ -1,27 +1,61 @@
 #include "PlayerGui.h"
 
+juce::String PlayerGui::formatTime(double seconds)
+{
+    if (seconds < 0.0) seconds = 0.0;
+    int totalSeconds = (int)std::round(seconds);
+    int minutes = totalSeconds / 60;
+    int remainingSeconds = totalSeconds % 60;
+    return juce::String::formatted("%02d:%02d", minutes, remainingSeconds);
+}
+
 PlayerGui::PlayerGui()
 {
-    addAndMakeVisible(volumeSlider);
-    volumeSlider.setRange(0.0, 1.0, 0.01);
-    volumeSlider.setValue(0.5);
-    volumeSlider.addListener(this);
-
-    for (auto* btn : { &loadButton, &playButton, &pauseButton, &goToStartButton, &goToEndButton, &muteButton, &loopButton })
+    // Buttons
+    for (auto* btn : { &loadButton, &playButton, &pauseButton, &goToStartButton,
+                       &goToEndButton, &muteButton, &loopButton })
     {
         addAndMakeVisible(*btn);
         btn->addListener(this);
     }
 
-    addAndMakeVisible(playlistBox);
-    playlistBox.setRowHeight(25);
+    addAndMakeVisible(volumeSlider);
+    volumeSlider.setRange(0.0, 1.0, 0.01);
+    volumeSlider.setValue(0.5);
+    volumeSlider.addListener(this);
+
+    addAndMakeVisible(positionSlider);
+    positionSlider.setSliderStyle(juce::Slider::TwoValueHorizontal);
+    positionSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    positionSlider.setRange(0.0, 100.0, 0.01);
+    positionSlider.setMinAndMaxValues(0.0, 100.0);
+    positionSlider.addListener(this);
+
+    addAndMakeVisible(posSlide);
+    posSlide.setRange(0.0, 1.0, 0.001);
+    posSlide.setValue(0.0);
+    posSlide.addListener(this);
+    posSlide.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+
+    addAndMakeVisible(timeDisplayLabel);
+    timeDisplayLabel.setJustificationType(juce::Justification::centred);
+    timeDisplayLabel.setFont(juce::Font(14.0f));
+    timeDisplayLabel.setText("00:00 / 00:00", juce::dontSendNotification);
 
     addAndMakeVisible(metadataLabel);
     metadataLabel.setJustificationType(juce::Justification::centredLeft);
     metadataLabel.setText("No file loaded", juce::dontSendNotification);
+
+    addAndMakeVisible(playlistBox);
+    playlistBox.setRowHeight(25);
+
+    startTimerHz(30);
 }
 
-PlayerGui::~PlayerGui() {}
+PlayerGui::~PlayerGui()
+{
+    stopTimer();
+}
 
 void PlayerGui::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
@@ -54,10 +88,17 @@ void PlayerGui::resized()
     loopButton.setBounds(550, y, buttonWidth, buttonHeight);
 
     y += 40;
-    metadataLabel.setBounds(10, y, getWidth() - 20, 30);
+    posSlide.setBounds(10, y, getWidth() - 100, 25);
+    timeDisplayLabel.setBounds(getWidth() - 90, y, 80, 25);
+
+    y += 30;
+    positionSlider.setBounds(10, y, getWidth() - 100, 25);
 
     y += 40;
-    playlistBox.setBounds(10, y, getWidth() - 20, 120);
+    metadataLabel.setBounds(10, y, getWidth() - 20, 25);
+
+    y += 35;
+    playlistBox.setBounds(10, y, getWidth() - 20, getHeight() - y - 10);
 }
 
 void PlayerGui::buttonClicked(juce::Button* button)
@@ -86,8 +127,15 @@ void PlayerGui::buttonClicked(juce::Button* button)
         int selectedRow = playlistBox.getSelectedRow();
         if (selectedRow >= 0 && selectedRow < playlistFiles.size())
         {
-            playerAudio.LoadFile(playlistFiles[selectedRow]);
-            updateMetadata(playlistFiles[selectedRow]);
+            if (playerAudio.LoadFile(playlistFiles[selectedRow]))
+            {
+                updateMetadata(playlistFiles[selectedRow]);
+
+                double length = playerAudio.getLength();
+                positionSlider.setRange(0.0, length, 0.01);
+                positionSlider.setMinAndMaxValues(0.0, length);
+                posSlide.setRange(0.0, length, 0.01);
+            }
         }
         playerAudio.play();
         pauseButton.setButtonText("Pause");
@@ -112,7 +160,6 @@ void PlayerGui::buttonClicked(juce::Button* button)
         playerAudio.stop();
         playerAudio.setPosition(0.0);
         playerAudio.play();
-        pauseButton.setButtonText("Pause");
     }
     else if (button == &goToEndButton)
     {
@@ -136,6 +183,27 @@ void PlayerGui::sliderValueChanged(juce::Slider* slider)
 {
     if (slider == &volumeSlider && !isMuted)
         playerAudio.setGain((float)volumeSlider.getValue());
+
+    else if (slider == &posSlide && slider->isMouseButtonDown())
+        playerAudio.setPosition(posSlide.getValue());
+}
+
+void PlayerGui::timerCallback()
+{
+    double currentPos = playerAudio.getPosition();
+    double length = playerAudio.getLength();
+
+    double a = positionSlider.getMinValue();
+    double b = positionSlider.getMaxValue();
+
+    if (playerAudio.isPlaying() && b > a && currentPos >= b)
+        playerAudio.setPosition(a);
+
+    if (!posSlide.isMouseButtonDown())
+        posSlide.setValue(currentPos, juce::dontSendNotification);
+
+    timeDisplayLabel.setText(formatTime(currentPos) + " / " + formatTime(length),
+        juce::dontSendNotification);
 }
 
 int PlayerGui::getNumRows() { return playlistFiles.size(); }
@@ -167,10 +235,13 @@ void PlayerGui::updateMetadata(const juce::File& file)
         auto title = reader->metadataValues["Title"];
         auto artist = reader->metadataValues["Artist"];
         double duration = reader->lengthInSamples / reader->sampleRate;
+
         if (title.isNotEmpty()) info += "Title: " + title + "   ";
         else info += "File: " + file.getFileName() + "   ";
+
         if (artist.isNotEmpty()) info += "Artist: " + artist + "   ";
         info += "Duration: " + juce::String(duration, 2) + " sec";
+
         metadataLabel.setText(info, juce::dontSendNotification);
         delete reader;
     }
