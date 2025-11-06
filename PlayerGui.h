a4656474c144b52e616324a6ab09dcc5c43adbe3
Amore20240387 <20240387@stud.fci-cu.edu.eg>
2025-11-07 01:28:17 +0200

    Update PlayerGui.h

--- PlayerGui.h
+++ PlayerGui.h
@@ -14,6 +14,7 @@ public:
     ~PlayerGui() override;
 
     void resized() override;
+    void paint(juce::Graphics&) override;
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& buffer);
     void releaseResources();
@@ -21,20 +22,26 @@ public:
 private:
     PlayerAudio playerAudio;
 
-    juce::TextButton loadButton{ "Load" };
+    juce::TextButton loadButton{ "Load File" };
     juce::TextButton playButton{ "Play" };
     juce::TextButton pauseButton{ "Pause" };
-    juce::TextButton goToStartButton{ "Restart" };
+    juce::TextButton restartButton{ "Restart" };
     juce::TextButton goToEndButton{ "End" };
     juce::TextButton muteButton{ "Mute" };
     juce::TextButton loopButton{ "Loop" };
+    juce::TextButton setAButton{ "Set A" };
+    juce::TextButton setBButton{ "Set B" };
+    juce::TextButton clearLoopButton{ "Clear Loop" };
 
     juce::Slider volumeSlider;
-    juce::Slider positionSlider; // A–B loop
-    juce::Slider posSlide;       // Playhead slider
+    juce::Slider speedSlider;
+    juce::Slider positionSlider;
+    juce::Slider progressBar;
 
     juce::Label timeDisplayLabel;
     juce::Label metadataLabel;
+    juce::Label loopLabel;
+    juce::Label progressLabel;
 
     juce::ListBox playlistBox{ "Playlist", this };
     juce::Array<juce::File> playlistFiles;
@@ -42,6 +49,24 @@ private:
     std::unique_ptr<juce::FileChooser> fileChooser;
     double savedPosition = 0.0;
     bool isMuted = false;
+    bool fileLoaded = false;
+    float lastVolume = 0.7f;
+
+    double loopStart = 0.0;
+    double loopEnd = 0.0;
+    bool isLoopSegmentSet = false;
+    bool isLoopingSegment = false;
+
+    juce::AudioFormatManager thumbnailFormatManager;
+    juce::AudioThumbnailCache thumbnailCache{ 5 };
+    juce::AudioThumbnail thumbnail{ 512, thumbnailFormatManager, thumbnailCache };
+
+    juce::Colour bgColor{ juce::Colour::fromRGB(30, 33, 40) };
+    juce::Colour primaryColor{ juce::Colour::fromRGB(97, 175, 239) };
+    juce::Colour accentColor{ juce::Colour::fromRGB(255, 159, 67) };
+    juce::Colour waveColor{ juce::Colour::fromRGB(120, 195, 255) };
+    juce::Colour textColor{ juce::Colour::fromRGB(240, 240, 240) };
+    juce::Colour loopColor{ juce::Colour::fromRGB(255, 100, 100) };
 
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
@@ -51,8 +76,10 @@ private:
     void paintListBoxItem(int rowNumber, juce::Graphics& g, int width, int height, bool rowIsSelected) override;
     void selectedRowsChanged(int lastRowSelected) override;
     void updateMetadata(const juce::File& file);
-
     juce::String formatTime(double seconds);
+    void loadWaveform(const juce::File& file);
+    void loadFileIntoPlayer(const juce::File& file);
+    void updateLoopDisplay();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGui)
 };
