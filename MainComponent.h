31b99f66ea80ac05bcc68b0db4b71983f45e6bc0
Amore20240387 <20240387@stud.fci-cu.edu.eg>
2025-11-07 13:34:26 +0200

    Update MainComponent.h

--- MainComponent.h
+++ MainComponent.h
@@ -2,7 +2,10 @@
 #include <JuceHeader.h>
 #include "PlayerGui.h"
 
-class MainComponent : public juce::AudioAppComponent
+class MainComponent : public juce::AudioAppComponent,
+    public juce::Button::Listener,
+    public juce::ComboBox::Listener,
+    public juce::Slider::Listener
 {
 public:
     MainComponent();
@@ -12,11 +15,40 @@ public:
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
     void resized() override;
-
     void paint(juce::Graphics& g) override;
 
+    void buttonClicked(juce::Button* button) override;
+    void comboBoxChanged(juce::ComboBox* comboBox) override;
+    void sliderValueChanged(juce::Slider* slider) override;
+
 private:
-    PlayerGui playerGui;
+    PlayerGui playerGui1;
+    PlayerGui playerGui2;
+    juce::TabbedComponent tabbedComponent{ juce::TabbedButtonBar::TabsAtTop };
+
+    juce::TextButton mixButton{ "Mix Mode" };
+    juce::TextButton effectsButton{ "Effects" };
+    juce::ComboBox effectsComboBox;
+
+    juce::Slider player1MixSlider;
+    juce::Slider player2MixSlider;
+    juce::Label player1MixLabel;
+    juce::Label player2MixLabel;
+    juce::Label mixTitleLabel;
+
+    bool isMixMode = false;
+    bool effectsEnabled = false;
+
+    juce::AudioBuffer<float> tempBuffer;
+
+    void mixAudio(const juce::AudioSourceChannelInfo& bufferToFill);
+    void applyEffects(const juce::AudioSourceChannelInfo& bufferToFill);
+
+    void applyReverbEffect(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
+    void applyEchoEffect(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
+    void applyChorusEffect(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
+    void applyFlangerEffect(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
+    void applyLowPassFilter(juce::AudioBuffer<float>& buffer, int startSample, int numSamples);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
